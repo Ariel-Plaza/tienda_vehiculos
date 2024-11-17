6f35357db1a3a2e169d81e0675f12d32cd4bc9a7
Ariel Plaza <arielplazasalinas@MacBook-Pro-de-Ariel.local>
2024-11-17 02:46:27 -0300

    Creacion entorno, proyecto, aplicacion, models, superusuario y registro en administracion django

---
+++ vehiculo/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Vehiculos(models.Model):
+    marca = models.CharField(max_length=20, default='Ford')
+    modelo = models.CharField(max_length=100)
+    serial_carroceria = models.CharField(max_length=50)
+    serial_motor = models.CharField(max_length=50)
+    categoria = models.CharField(max_length=20, default='Particular')
+    precio = models.IntegerField()
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
